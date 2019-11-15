38f43a2d7b71ac30f51a262fac6434d73a7622ca
devavx <dev.aviralx@gmail.com>
2019-11-15 10:39:41 +0530

    Added auth models for Admin and User

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 // Authentication Routes
 Auth::routes();
 
-Route::get('/', [HomeController::class, 'index'])->middleware('auth');
+Route::get('/', [HomeController::class, 'index']);
