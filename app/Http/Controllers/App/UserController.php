5de04234e454660e4652a473ecefdc2bdbda32f3
devavx <dev.aviralx@gmail.com>
2019-11-20 18:56:57 +0530

    Improved structure of project

--- app/Http/Controllers/App/UserController.php
+++ app/Http/Controllers/App/UserController.php
@@ -9,12 +9,17 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends AppController {
 	public function index(int $id = null) {
-		$user = null;
-		if ($id == null) {
-			$user = Auth::user();
-		} else {
-			$user = User::find($id);
-		}
-		return new UserResource($user);
+		$users = User::all();
+		$index = 1;
+		$users->transform(function (User $user) use ($index) {
+			return [
+				'id' => $user->getId(),
+				'name' => $user->getName(),
+				'mobile' => $user->getMobile(),
+				'email' => $user->getEmail(),
+				'index' => $index++,
+			];
+		});
+		return $users;
 	}
 }
