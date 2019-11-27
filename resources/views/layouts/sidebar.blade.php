fb6b8d3872400bdd5193c66c7914ee2d69fbe127
devavx <dev.aviralx@gmail.com>
2019-11-27 17:09:43 +0530

    Updates

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -22,43 +22,43 @@
 				</li>
 
 				<li>
-					<a href="{{route("users.all")}}" class="waves-effect"><i class="mdi mdi-account-circle mt-1"></i> <span> Users </span></a>
+					<a href="{{route("users.all")}}" class="waves-effect"><i class="mdi mdi-account-circle mt-1"></i> <span>Users </span></a>
 				</li>
 
 				<li class="menu-title">Shopping</li>
 
 				<li>
-					<a href="{{route("categories.all")}}" class="waves-effect"><i class="mdi mdi-cards mt-1"></i> <span> Categories </span></a>
+					<a href="{{route("categories.all")}}" class="waves-effect"><i class="mdi mdi-cards mt-1"></i> <span>Categories </span></a>
 				</li>
 
 				<li class="menu-title">Entertainment</li>
 
 				<li>
-					<a href="{{route("movies.all")}}" class="waves-effect"><i class="mdi mdi-movie" style="margin-top: 2px"></i> <span> Movies </span></a>
+					<a href="{{route("movies.all")}}" class="waves-effect"><i class="mdi mdi-movie" style="margin-top: 2px"></i> <span>Movies </span></a>
 				</li>
 
 				<li>
-					<a href="{{route("categories.all")}}" class="waves-effect"><i class="mdi mdi-step-forward mt-1"></i> <span> Series </span></a>
+					<a href="{{route("categories.all")}}" class="waves-effect"><i class="mdi mdi-step-forward mt-1"></i> <span>Series </span></a>
 				</li>
 
 				<li>
-					<a href="{{route("categories.all")}}" class="waves-effect"><i class="mdi mdi-television mt-1"></i> <span> Live TV </span></a>
+					<a href="{{route("categories.all")}}" class="waves-effect"><i class="mdi mdi-television mt-1"></i> <span>Live TV </span></a>
 				</li>
 
 				<li>
-					<a href="{{route("categories.all")}}" class="waves-effect"><i class="mdi mdi-server mt-1"></i> <span> Servers </span></a>
+					<a href="{{route("categories.all")}}" class="waves-effect"><i class="mdi mdi-server mt-1"></i> <span>Servers </span></a>
 				</li>
 
 				<li>
-					<a href="{{route("genres.index")}}" class="waves-effect"><i class="mdi mdi-view-headline mt-1"></i> <span> Genres </span></a>
+					<a href="{{route("genres.index")}}" class="waves-effect"><i class="mdi mdi-view-headline mt-1"></i> <span>Genres </span></a>
 				</li>
 
 				<li>
-					<a href="{{route("notifications.create")}}" class="waves-effect"><i class="mdi mdi-near-me mt-1"></i> <span> Notifications </span></a>
+					<a href="{{route("notifications.create")}}" class="waves-effect"><i class="mdi mdi-near-me mt-1"></i> <span>Notifications </span></a>
 				</li>
 
 				<li>
-					<a href="{{route("categories.all")}}" class="waves-effect"><i class="mdi mdi-settings mt-1"></i> <span> Settings </span></a>
+					<a href="{{route("categories.all")}}" class="waves-effect"><i class="mdi mdi-settings mt-1"></i> <span>Settings </span></a>
 				</li>
 			</ul>
 		</div>
