2768142f9f21d7497a7cc67a2dfed54fd285cc58
devavx <dev.aviralx@gmail.com>
2019-12-16 18:30:11 +0530

    Changes HTTP status codes to internal status codes

--- app/Classes/Builders/ResponseBuilder.php
+++ app/Classes/Builders/ResponseBuilder.php
@@ -10,57 +10,51 @@ use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Support\Facades\Lang;
 
 /**
  * Builder class to create responses, supports chained method class for easy one line responses.
  * @package App\Utils
  */
-class ResponseBuilder {
-
-	/**
-	 * Enables response logging for each response generated through build method.
-	 * @param bool $enable
-	 */
-	public static function enableResponseLogging(bool $enable) {
-		self::$loggingEnabled = $enable;
-	}
+class ResponseBuilder{
 
 	/**
 	 * @var bool
 	 */
 	private static $loggingEnabled = true;
-
 	/**
 	 * @var string
 	 */
 	private $message;
-
 	/**
 	 * @var int
 	 */
 	private $status = 0;
-
 	/**
 	 * @var array
 	 */
 	private $items = [];
-
 	/**
 	 * @var int
 	 */
 	private $statusCode = 200;
-
 	/**
 	 * @var int
 	 */
 	private $requestId = -1;
 
 	/**
+	 * Enables response logging for each response generated through build method.
+	 * @param bool $enable
+	 */
+	public static function enableResponseLogging(bool $enable){
+		self::$loggingEnabled = $enable;
+	}
+
+	/**
 	 * Returns an instance of AppResponse setting its status to 1.
 	 * @return ResponseBuilder
 	 */
-	public static function asSuccess() {
+	public static function asSuccess(){
 		$object = new ResponseBuilder();
 		$object->message = 'Success';
 		$object->status = 1;
@@ -71,7 +65,7 @@ class ResponseBuilder {
 	 * Returns an instance of AppResponse setting its status to 0.
 	 * @return ResponseBuilder
 	 */
-	public static function asFailure() {
+	public static function asFailure(){
 		$object = new ResponseBuilder();
 		$object->message = 'Failed';
 		$object->status = 0;
@@ -82,7 +76,7 @@ class ResponseBuilder {
 	 * Returns an instance of AppResponse setting its status to 0.
 	 * @return ResponseBuilder
 	 */
-	public static function asError() {
+	public static function asError(){
 		$object = new ResponseBuilder();
 		$object->message = 'Failed';
 		$object->status = 0;
@@ -90,7 +84,7 @@ class ResponseBuilder {
 		return $object;
 	}
 
-	public static function instance() {
+	public static function instance(){
 		return new ResponseBuilder();
 	}
 
@@ -99,7 +93,7 @@ class ResponseBuilder {
 	 * @param $message string|callable|Validator
 	 * @return ResponseBuilder
 	 */
-	public function message($message) {
+	public function message($message){
 		if (is_callable($message)) {
 			$this->message = call_user_func($message);
 		}
@@ -118,12 +112,12 @@ class ResponseBuilder {
 	 * @param null $value
 	 * @return $this
 	 */
-	public function setValue(string $key, $value = null) {
+	public function setValue(string $key, $value = null){
 		$this->items[$key] = $value;
 		return $this;
 	}
 
-	public function setResource(JsonResource $resource, string $key = 'data', Request $request = null) {
+	public function setResource(JsonResource $resource, string $key = 'data', Request $request = null){
 		$this->items[$key] = $resource->toArray($request);
 		return $this;
 	}
@@ -133,7 +127,7 @@ class ResponseBuilder {
 	 * @param string $key Name of list (default is list)
 	 * @return $this
 	 */
-	public function setEmptyList(string $key = 'list') {
+	public function setEmptyList(string $key = 'list'){
 		$this->items[$key] = [];
 		return $this;
 	}
@@ -143,7 +137,7 @@ class ResponseBuilder {
 	 * @param int $statusCode
 	 * @return $this
 	 */
-	public function status(int $statusCode) {
+	public function status(int $statusCode){
 		$this->statusCode = $statusCode;
 		return $this;
 	}
@@ -152,20 +146,21 @@ class ResponseBuilder {
 	 * Builds an returns a JsonResponse with the specified response data.
 	 * @return JsonResponse
 	 */
-	public function send() {
+	public function send(){
 		$response = [];
+		$response['status'] = $this->statusCode;
 		$response['message'] = $this->message;
 		foreach ($this->items as $key => $value) {
 			$response[$key] = $value;
 		}
-		return response()->json($response, $this->statusCode);
+		return response()->json($response);
 	}
 
 	/**
 	 * Builds an returns a JsonResponse with the specified response data.
 	 * @return array
 	 */
-	public function buildArray() {
+	public function buildArray(){
 		$response = [];
 		$response['status'] = $this->status;
 		$response['message'] = $this->message;
