16d722cf8dbf1fc1214c3f5843e5565bb7830897
devavx <dev.aviralx@gmail.com>
2019-12-16 15:02:51 +0530

    Changed customer auth validation rule to allow registration with one field

--- database/migrations/2014_10_12_000000_create_customers_table.php
+++ database/migrations/2014_10_12_000000_create_customers_table.php
@@ -1,22 +1,21 @@
 <?php
 
-use App\Interfaces\Roles;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCustomersTable extends Migration {
+class CreateCustomersTable extends Migration{
 	/**
 	 * Run the migrations.
 	 *
 	 * @return void
 	 */
-	public function up() {
-		Schema::create('customers', function (Blueprint $table) {
+	public function up(){
+		Schema::create('customers', function (Blueprint $table){
 			$table->bigIncrements('id');
 			$table->string('name');
-			$table->string('email')->unique();
-			$table->string('mobile')->unique();
+			$table->string('email')->unique()->nullable();
+			$table->string('mobile')->unique()->nullable();
 			$table->timestamp('email_verified_at')->nullable();
 			$table->string('password');
 			$table->boolean('active')->default(1);
@@ -30,7 +29,7 @@ class CreateCustomersTable extends Migration {
 	 *
 	 * @return void
 	 */
-	public function down() {
+	public function down(){
 		Schema::dropIfExists('customers');
 	}
 }
