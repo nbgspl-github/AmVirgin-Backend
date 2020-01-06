33a9ce091ec5136f82f027fc5d043d7f3f4b08ad
devavx <dev.aviralx@gmail.com>
2020-01-06 15:05:41 +0530

    Updates

--- resources/views/admin/app/partials/scripts.blade.php
+++ resources/views/admin/app/partials/scripts.blade.php
@@ -17,14 +17,7 @@
 <script src="{{asset("admin/plugins/datatables/jquery.dataTables.min.js")}}"></script>
 <script src="{{asset("admin/plugins/datatables/dataTables.bootstrap4.min.js")}}"></script>
 <!-- Buttons examples -->
-<script src="{{asset("admin/plugins/datatables/dataTables.buttons.min.js")}}"></script>
-<script src="{{asset("admin/plugins/datatables/buttons.bootstrap4.min.js")}}"></script>
-<script src="{{asset("admin/plugins/datatables/jszip.min.js")}}"></script>
-<script src="{{asset("admin/plugins/datatables/pdfmake.min.js")}}"></script>
-<script src="{{asset("admin/plugins/datatables/vfs_fonts.js")}}"></script>
-<script src="{{asset("admin/plugins/datatables/buttons.html5.min.js")}}"></script>
-<script src="{{asset("admin/plugins/datatables/buttons.print.min.js")}}"></script>
-<script src="{{asset("admin/plugins/datatables/buttons.colVis.min.js")}}"></script>
+
 <!-- Responsive examples -->
 <script src="{{asset("admin/plugins/datatables/dataTables.responsive.min.js")}}"></script>
 <script src="{{asset("admin/plugins/datatables/responsive.bootstrap4.min.js")}}"></script>
