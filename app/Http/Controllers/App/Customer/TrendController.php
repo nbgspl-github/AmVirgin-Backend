c500d57ff1312a6aff53d3d2b93aed5362341cdf
devavx <dev.aviralx@gmail.com>
2020-01-07 15:33:58 +0530

    Updates

--- app/Http/Controllers/App/Customer/TrendController.php
+++ app/Http/Controllers/App/Customer/TrendController.php
@@ -10,38 +10,47 @@ use App\Models\Video;
 use App\Traits\FluentResponse;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
+use Throwable;
 
 class TrendController extends ResourceController{
 	use FluentResponse;
 
 	public function index(){
-		$trending = Video::where([
-			['trending', true],
-			['rank', '>', 0],
-		])->orderBy('rank', 'DESC')->get();
-		$trending->transform(function (Video $video){
-			return new TrendingPicksVideoResource($video);
-		});
+		$response = $this->response();
+		try {
+			$trending = Video::where([
+				['trending', true],
+				['rank', '>', 0],
+			])->orderBy('rank', 'DESC')->get();
+			$trending->transform(function (Video $video){
+				return new TrendingPicksVideoResource($video);
+			});
 
-		$justAdded = Video::latest()->take(10)->get();
-		$justAdded->transform(function (Video $video){
-			return new JustAddedVideoResource($video);
-		});
+			$justAdded = Video::latest()->take(10)->get();
+			$justAdded->transform(function (Video $video){
+				return new JustAddedVideoResource($video);
+			});
 
-		$topPicks = Video::where([
-			['topPick', true],
-		])->orderBy('created_at', 'DESC')->get();
-		$topPicks->transform(function (Video $video){
-			return new TopPicksVideoResource($video);
-		});
+			$topPicks = Video::where([
+				['topPick', true],
+			])->orderBy('created_at', 'DESC')->get();
+			$topPicks->transform(function (Video $video){
+				return new TopPicksVideoResource($video);
+			});
 
-		$payload = [
-			'trendingPicks' => $trending->all(),
-			'justAdded' => $justAdded->all(),
-			'topPicks' => $topPicks->all(),
-		];
-
-		return $this->success()->status(HttpOkay)->setValue('data', $payload)->send();
+			$payload = [
+				'trendingPicks' => $trending->all(),
+				'justAdded' => $justAdded->all(),
+				'topPicks' => $topPicks->all(),
+			];
+			$response->status(HttpOkay)->setValue('data', $payload);
+		}
+		catch (Throwable $exception) {
+			$response->status(HttpServerError)->setValue('data');
+		}
+		finally {
+			return $response->send();
+		}
 	}
 
 	protected function parentProvider(){
