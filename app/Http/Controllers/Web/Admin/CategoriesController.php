c4a514725cbc3a87eb1b98df0d6107018fa1dfbb
devavx <dev.aviralx@gmail.com>
2020-01-22 13:07:10 +0530

    Updates

--- app/Http/Controllers/Web/Admin/CategoriesController.php
+++ app/Http/Controllers/Web/Admin/CategoriesController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Web\Admin;
 
-use App\Category;
+use App\Models\Category;
 use App\Exceptions\ValidationException;
 use App\Http\Controllers\BaseController;
 use App\Interfaces\Directories;
