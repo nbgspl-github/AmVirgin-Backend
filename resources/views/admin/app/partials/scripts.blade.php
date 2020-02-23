2d47adff1bdeafc5154090b212e52f6c3bf77725
devavx <dev.aviralx@gmail.com>
2020-02-23 21:51:01 +0530

    Updates

--- resources/views/admin/app/partials/scripts.blade.php
+++ resources/views/admin/app/partials/scripts.blade.php
@@ -1,55 +1,55 @@
 <!-- jQuery  -->
-<script src="{{asset("admin/js/jquery.min.js")}}"></script>
-<script src="{{asset("admin/js/bootstrap.bundle.min.js")}}"></script>
-<script src="{{asset("admin/js/modernizr.min.js")}}"></script>
-<script src="{{asset("admin/js/detect.js")}}"></script>
-<script src="{{asset("admin/js/fastclick.js")}}"></script>
-<script src="{{asset("admin/js/jquery.slimscroll.js")}}"></script>
-<script src="{{asset("admin/js/jquery.blockUI.js")}}"></script>
-<script src="{{asset("admin/js/waves.js")}}"></script>
-<script src="{{asset("admin/js/jquery.nicescroll.js")}}"></script>
-<script src="{{asset("admin/js/jquery.scrollTo.min.js")}}"></script>
+<script src="{{asset("assets/admin/js/jquery.min.js")}}"></script>
+<script src="{{asset("assets/admin/js/bootstrap.bundle.min.js")}}"></script>
+<script src="{{asset("assets/admin/js/modernizr.min.js")}}"></script>
+<script src="{{asset("assets/admin/js/detect.js")}}"></script>
+<script src="{{asset("assets/admin/js/fastclick.js")}}"></script>
+<script src="{{asset("assets/admin/js/jquery.slimscroll.js")}}"></script>
+<script src="{{asset("assets/admin/js/jquery.blockUI.js")}}"></script>
+<script src="{{asset("assets/admin/js/waves.js")}}"></script>
+<script src="{{asset("assets/admin/js/jquery.nicescroll.js")}}"></script>
+<script src="{{asset("assets/admin/js/jquery.scrollTo.min.js")}}"></script>
 
 <!-- Parsley js -->
-<script src="{{asset("admin/plugins/parsleyjs/parsley.min.js")}}"></script>
+<script src="{{asset("assets/admin/plugins/parsleyjs/parsley.min.js")}}"></script>
 
 <!-- Required datatable js -->
-<script src="{{asset("admin/plugins/datatables/jquery.dataTables.min.js")}}"></script>
-<script src="{{asset("admin/plugins/datatables/dataTables.bootstrap4.min.js")}}"></script>
+<script src="{{asset("assets/admin/plugins/datatables/jquery.dataTables.min.js")}}"></script>
+<script src="{{asset("assets/admin/plugins/datatables/dataTables.bootstrap4.min.js")}}"></script>
 <!-- Buttons examples -->
 
 <!-- Responsive examples -->
-<script src="{{asset("admin/plugins/datatables/dataTables.responsive.min.js")}}"></script>
-<script src="{{asset("admin/plugins/datatables/responsive.bootstrap4.min.js")}}"></script>
+<script src="{{asset("assets/admin/plugins/datatables/dataTables.responsive.min.js")}}"></script>
+<script src="{{asset("assets/admin/plugins/datatables/responsive.bootstrap4.min.js")}}"></script>
 
-<script src="{{asset('admin/js/toastr.min.js')}}"></script>
+<script src="{{asset('assets/admin/js/toastr.min.js')}}"></script>
 
-<script src="{{asset("admin/js/axios.js")}}"></script>
+<script src="{{asset("assets/admin/js/axios.js")}}"></script>
 
 <!-- Alertify js -->
-<script src="{{asset('admin/plugins/alertify/js/alertify.js')}}"></script>
+<script src="{{asset('assets/admin/plugins/alertify/js/alertify.js')}}"></script>
 
 <!-- jQuery Peity -->
-<script src="{{asset('admin/js/circle-progress.js')}}"></script>
+<script src="{{asset('assets/admin/js/circle-progress.js')}}"></script>
 
 {{--Anime JS--}}
-<script src="{{asset('admin/js/anime.js')}}"></script>
+<script src="{{asset('assets/admin/js/anime.js')}}"></script>
 
 <!-- Dropzone js -->
-<script src="{{asset('admin/plugins/dropzone/dist/dropzone.js')}}"></script>
+<script src="{{asset('assets/admin/plugins/dropzone/dist/dropzone.js')}}"></script>
 
 <script src="https://vjs.zencdn.net/7.6.6/video.js"></script>
 
 <!--Bootstrap Duration Picker-->
-<script src="{{asset('admin/js/bootstrap-duration-picker.js')}}"></script>
+<script src="{{asset('assets/admin/js/bootstrap-duration-picker.js')}}"></script>
 
-<script src="{{asset('admin/js/jquery.caret.min.js')}}"></script>
-<script src="{{asset('admin/js/jquery.tag-editor.min.js')}}"></script>
+<script src="{{asset('assets/admin/js/jquery.caret.min.js')}}"></script>
+<script src="{{asset('assets/admin/js/jquery.tag-editor.min.js')}}"></script>
 
 <!--Mustache Js-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/3.1.0/mustache.js"></script>
 
-<script src="{{asset('admin/js/duration-picker.js')}}"></script>
+<script src="{{asset('assets/admin/js/duration-picker.js')}}"></script>
 
 {{--<!-- App js -->--}}
-<script src="{{asset("admin/js/app.js")}}"></script>
+<script src="{{asset("assets/admin/js/app.js")}}"></script>
--- resources/views/admin/app/partials/styles.blade.php
+++ resources/views/admin/app/partials/styles.blade.php
@@ -1,30 +1,30 @@
 <!-- DataTables -->
-<link href="{{asset('admin/plugins/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet" type="text/css"/>
-<link href="{{asset('admin/plugins/datatables/buttons.bootstrap4.min.css')}}" rel="stylesheet" type="text/css"/>
+<link href="{{asset('assets/admin/plugins/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet" type="text/css"/>
+<link href="{{asset('assets/admin/plugins/datatables/buttons.bootstrap4.min.css')}}" rel="stylesheet" type="text/css"/>
 <!-- Responsive datatable examples -->
-<link href="{{asset('admin/plugins/datatables/responsive.bootstrap4.min.css')}}" rel="stylesheet" type="text/css"/>
+<link href="{{asset('assets/admin/plugins/datatables/responsive.bootstrap4.min.css')}}" rel="stylesheet" type="text/css"/>
 <!-- Alertify css -->
-<link href="{{asset('admin/plugins/alertify/css/alertify.css')}}" rel="stylesheet" type="text/css">
-<link href="{{asset('admin/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
-<link href="{{asset('admin/css/icons.css')}}" rel="stylesheet" type="text/css">
-<link href="{{asset('admin/css/style.css')}}" rel="stylesheet" type="text/css">
-<link href="{{asset('admin/css/custom.css')}}" rel="stylesheet" type="text/css">
+<link href="{{asset('assets/admin/plugins/alertify/css/alertify.css')}}" rel="stylesheet" type="text/css">
+<link href="{{asset('assets/admin/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
+<link href="{{asset('assets/admin/css/icons.css')}}" rel="stylesheet" type="text/css">
+<link href="{{asset('assets/admin/css/style.css')}}" rel="stylesheet" type="text/css">
+<link href="{{asset('assets/admin/css/custom.css')}}" rel="stylesheet" type="text/css">
 
 <!--Morris Chart CSS -->
-<link rel="stylesheet" href="{{asset('admin/css/percircle.css')}}">
+<link rel="stylesheet" href="{{asset('assets/admin/css/percircle.css')}}">
 
 <link href="https://vjs.zencdn.net/7.6.6/video-js.css" rel="stylesheet"/>
 
 <!--Bootstrap Duration Picker-->
-<link rel="stylesheet" href="{{asset('admin/css/bootstrap-duration-picker.css')}}">
+<link rel="stylesheet" href="{{asset('assets/admin/css/bootstrap-duration-picker.css')}}">
 
-<link rel="stylesheet" href="{{asset('admin/css/jquery.tag-editor.css')}}">
+<link rel="stylesheet" href="{{asset('assets/admin/css/jquery.tag-editor.css')}}">
 
-<link rel="stylesheet" href="{{asset('admin/css/tageditor.css')}}">
+<link rel="stylesheet" href="{{asset('assets/admin/css/tageditor.css')}}">
 
-<link rel="stylesheet" href="{{asset('admin/css/animate.css')}}">
+<link rel="stylesheet" href="{{asset('assets/admin/css/animate.css')}}">
 
-<link rel="stylesheet" href="{{asset('admin/css/loading.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/admin/css/loading.min.css')}}">
 
 <!-- Dropzone css -->
-<link href="{{asset('admin/plugins/dropzone/dist/dropzone.css')}}" rel="stylesheet" type="text/css">
+<link href="{{asset('assets/admin/plugins/dropzone/dist/dropzone.css')}}" rel="stylesheet" type="text/css">
