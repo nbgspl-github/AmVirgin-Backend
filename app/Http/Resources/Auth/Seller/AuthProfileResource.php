e4a72da3fd151cc5bf8c42ff0c5b46d19185641d
aditya <adityashivhare@younggeeks.in>
2020-02-29 17:16:28 +0530

    Updates

--- app/Http/Resources/Auth/Seller/AuthProfileResource.php
+++ app/Http/Resources/Auth/Seller/AuthProfileResource.php
@@ -8,9 +8,16 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class AuthProfileResource extends JsonResource {
 	public function toArray($request) {
 		return [
+			'key' => $this->id,
 			'name' => $this->name,
+			'businessName' => $this->businessName,
+			'description' => $this->description,
 			'email' => $this->email,
 			'mobile' => $this->mobile,
+			'state' => $this->state,
+			'city' => $this->city,
+			'rating' => $this->rating,
+			'alternateMobile' => $this->alternateMobile,
 			'avatar' => SecuredDisk::existsUrl($this->avatar),
 		];
 	}
--- app/Models/Auth/Seller.php
+++ app/Models/Auth/Seller.php
@@ -116,4 +116,12 @@ class Seller extends Authenticatable implements JWTSubject {
 	public function getJWTCustomClaims() {
 		return [];
 	}
+
+	public function city() {
+		$this->belongsTo('App\Models\City', 'cityId', 'id');
+	}
+
+	public function state() {
+		$this->belongsTo('App\Models\State', 'stateId', 'id');
+	}
 }
