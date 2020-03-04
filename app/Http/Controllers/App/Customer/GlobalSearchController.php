4dd8392675fae29e39596ffce4bc0a89b6a6158d
Aviral Singh <dev.aviralx@gmail.com>
2020-03-04 18:56:11 +0530

    Updates

--- app/Http/Controllers/App/Customer/GlobalSearchController.php
+++ app/Http/Controllers/App/Customer/GlobalSearchController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers\App\Customer;
 use App\Constants\PageSectionType;
 use App\Http\Controllers\Web\ExtendedResourceController;
 use App\Interfaces\Tables;
+use App\Traits\ValidatesRequest;
 use Illuminate\Validation\Rule;
 use Throwable;
 
 class GlobalSearchController extends ExtendedResourceController {
+	use ValidatesRequest;
 	protected array $rules;
 
 	public function __construct() {
@@ -24,6 +26,7 @@ class GlobalSearchController extends ExtendedResourceController {
 
 	public function search() {
 		try {
+			$validated = (object)$this->requestValid(request(), $this->rules['search']);
 
 		}
 		catch (Throwable $exception) {
