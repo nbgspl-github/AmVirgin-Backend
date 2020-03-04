9850d5fd36db194fafc673cf74329256a7c6ae85
Aviral Singh <dev.aviralx@gmail.com>
2020-03-04 16:36:39 +0530

    Updates

--- app/Resources/Shop/Customer/HomePage/BrandsInFocusResource.php
+++ app/Resources/Shop/Customer/HomePage/BrandsInFocusResource.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class BrandsInFocusResource extends JsonResource {
 	public function toArray($request) {
 		return [
+			'id' => $this->id,
 			'name' => $this->name,
 			'description' => $this->description,
 			'poster' => SecuredDisk::existsUrl($this->poster),
