07b481d6ae40f56e3eb6ef6e52de462e133fa505
Aviral Singh <dev.aviralx@gmail.com>
2020-03-05 13:52:28 +0530

    Updates

--- app/Resources/Shop/Customer/HomePage/TrendingNowResource.php
+++ app/Resources/Shop/Customer/HomePage/TrendingNowResource.php
@@ -9,7 +9,7 @@ class TrendingNowResource extends JsonResource {
 	public function toArray($request) {
 		return [
 			'id' => $this->id,
-			'title' => $this->name,
+			'title' => $this->title,
 			'description' => $this->description,
 			'poster' => SecuredDisk::existsUrl($this->poster),
 		];
