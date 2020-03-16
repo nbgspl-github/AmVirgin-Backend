5ba64406462cd807a0fca13114d144502e76f1c7
younggeeksin <raju@younggeeks.in>
2020-03-16 11:56:05 +0530

    Add Column in Products table

--- database/migrations/2020_03_14_174136_add_column_to_products.php
+++ database/migrations/2020_03_14_174136_add_column_to_products.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateColumnInProducts extends Migration
+class AddColumnToProducts extends Migration
 {
     /**
      * Run the migrations.
@@ -14,12 +14,12 @@ class UpdateColumnInProducts extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('domesticwarranty ')->nullable()->after('sellingPrice');
-            $table->string('internationalWarranty  ')->nullable()->after('domesticwarranty');
-            $table->string('warrantySummary ')->nullable()->after('internationalWarranty');
-            $table->string('warrantyServiceType  ')->nullable()->after('warrantySummary');
-            $table->string('coveredInWarranty    ')->nullable()->after('warrantyServiceType');
-            $table->string('notCoveredInWarranty   ')->nullable()->after('coveredInWarranty');
+            $table->text('domesticwarranty')->nullable()->after('sellingPrice');
+            $table->text('internationalWarranty')->nullable()->after('domesticwarranty');
+            $table->text('warrantySummary')->nullable()->after('internationalWarranty');
+            $table->text('warrantyServiceType')->nullable()->after('warrantySummary');
+            $table->text('coveredInWarranty')->nullable()->after('warrantyServiceType');
+            $table->text('notCoveredInWarranty')->nullable()->after('coveredInWarranty');
         });
     }
 
