dc3a1b61844c5ea6a52544f781a1f8b6f32f872e
Aviral Singh <dev.aviralx@gmail.com>
2020-03-16 12:54:17 +0530

    Updates

--- database/migrations/2020_03_14_174136_add_column_to_products.php
+++ database/migrations/2020_03_14_174136_add_column_to_products.php
@@ -4,34 +4,31 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnToProducts extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::table('products', function (Blueprint $table) {
-            $table->text('domesticwarranty')->nullable()->after('sellingPrice');
-            $table->text('internationalWarranty')->nullable()->after('domesticwarranty');
-            $table->text('warrantySummary')->nullable()->after('internationalWarranty');
-            $table->text('warrantyServiceType')->nullable()->after('warrantySummary');
-            $table->text('coveredInWarranty')->nullable()->after('warrantyServiceType');
-            $table->text('notCoveredInWarranty')->nullable()->after('coveredInWarranty');
-        });
-    }
+class AddColumnToProducts extends Migration{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up(){
+		Schema::table('products', function (Blueprint $table){
+			$table->text('domesticWarranty')->nullable()->after('sellingPrice');
+			$table->text('internationalWarranty')->nullable()->after('domesticWarranty');
+			$table->text('warrantySummary')->nullable()->after('internationalWarranty');
+			$table->text('warrantyServiceType')->nullable()->after('warrantySummary');
+			$table->text('coveredInWarranty')->nullable()->after('warrantyServiceType');
+			$table->text('notCoveredInWarranty')->nullable()->after('coveredInWarranty');
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::table('products', function (Blueprint $table) {
-            $table->dropColumn(['domesticwarranty','internationalWarranty ','warrantySummary','warrantyServiceType','coveredInWarranty','notCoveredInWarranty  ']);
-        });
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down(){
+		Schema::table('products', function (Blueprint $table){
+			$table->dropColumn(['domesticWarranty', 'internationalWarranty ', 'warrantySummary', 'warrantyServiceType', 'coveredInWarranty', 'notCoveredInWarranty']);
+		});
+	}
 }
