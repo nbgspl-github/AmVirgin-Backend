f0c55abc8227746f5f7f00db2fa7af6c88df5087
Dheerendra <ddpwpareshan@gmail.com>
2020-03-24 12:01:30 +0530

    Forgot Pass

--- app/Http/Controllers/App/Seller/AuthController.php
+++ app/Http/Controllers/App/Seller/AuthController.php
@@ -11,6 +11,8 @@ use Throwable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Support\Facades\Password;
 
 class AuthController extends BaseAuthController {
 	protected $ruleSet;
@@ -92,6 +94,51 @@ class AuthController extends BaseAuthController {
 				}
 			}
 	    }
+
+	public function forgotPassword(Request $request)
+	{
+		$response = responseApp();
+
+	    $input = $request->all();
+	    $rules = array(
+	        'email' => "required|email",
+	    );
+	    $validator = Validator::make($input, $rules);
+	    if ($validator->fails()) {
+	        // $arr = array("status" => 400, "message" => $validator->errors()->first());
+	        $response->status(HttpInvalidRequestFormat)->message($validator->errors()->first());
+	    } else {
+	        try {
+	            $reset_response = Password::sendResetLink($request->only('email'), function (Message $message) {
+	                $message->subject($this->getEmailSubject());
+	            });
+	            switch ($reset_response) {
+	                case Password::RESET_LINK_SENT:
+	                    // return \Response::json(array("status" => 200, "message" => trans($response), "data" => array()));
+	            	return $response->status(HttpOkay)->message(trans($reset_response));
+	                case Password::INVALID_USER:
+	                    // return \Response::json(array("status" => 400, "message" => trans($response), "data" => array()));
+	                	return $response->status(HttpOkay)->message(trans($reset_response));
+	            }
+
+	        // return $response = Password::RESET_LINK_SENT
+	        //     ? response()->json(['status' => 'Success','message' => 'Reset Password Link Sent'],201)
+	        //     : response()->json(['status' => 'Fail','message' => 'Reset Link Could Not Be Sent'],401);
+
+	        } catch (ModelNotFoundException $exception) {
+	            // $arr = array("status" => 400, "message" => $ex->getMessage(), "data" => []);
+
+	            $response->status(HttpResourceNotFound)->message('Could not find seller for that key.');
+
+	        } catch (EThrowable $exception) {
+	           $response->status(HttpServerError)->message($exception->getMessage());
+				
+	        }
+	    }
+	    // return \Response::json($arr);
+	    return $response->send();
+
+	}
 
 	protected function authTarget(): string {
 		return Seller::class;
