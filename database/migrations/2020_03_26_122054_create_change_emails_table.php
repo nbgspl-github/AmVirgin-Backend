43b2324a4d88ccde1e39d045954be2764d1e63e2
Aviral Singh <dev.aviralx@gmail.com>
2020-03-26 18:25:57 +0530

    Updates

--- database/migrations/2020_03_26_122054_create_change_emails_table.php
+++ database/migrations/2020_03_26_122054_create_change_emails_table.php
@@ -4,30 +4,27 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateChangeEmailsTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('change_emails', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('email')->nullable();
-            $table->string('token')->nullable();
-            $table->timestamps();
-        });
-    }
+class CreateChangeEmailsTable extends Migration{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up(){
+		Schema::create('change-emails', function (Blueprint $table){
+			$table->bigIncrements('id');
+			$table->string('email')->nullable();
+			$table->string('token')->nullable();
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('change_emails');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down(){
+		Schema::dropIfExists('change-emails');
+	}
 }
