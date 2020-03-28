0a7d1625522982b0885b6809a0b0901495bd135a
Aviral Singh <dev.aviralx@gmail.com>
2020-03-28 18:50:51 +0530

    Updates

--- app/Contracts/DisplayableModel.php
+++ app/Contracts/DisplayableModel.php
@@ -9,7 +9,7 @@ interface DisplayableModel{
 	/**
 	 * Defines a set of conditions which decide whether a particular
 	 * model instance is eligible to be displayed on the front-end.
-	 * @return QueryProvider
+	 * @return Builder
 	 */
-	public static function displayable(): QueryProvider;
+	public static function displayable(): Builder;
 }
