3cf7a7ec0f6bd677d7543b746f05478b73f83c81
Aviral Singh <mr.aviralsingh@gmail.com>
2020-04-15 02:04:42 +0530

    Updates
    Signed-off-by: Aviral Singh <mr.aviralsingh@gmail.com>

--- app/Resources/Orders/Customer/OrderItemResource.php
+++ app/Resources/Orders/Customer/OrderItemResource.php
@@ -2,14 +2,13 @@
 
 namespace App\Resources\Orders\Customer;
 
-use App\Resources\Products\Customer\CatalogListResource;
-use App\Resources\Products\Customer\ProductResource;
+use App\Resources\Products\Customer\CartProductResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class OrderItemResource extends JsonResource{
 	public function toArray($request){
 		return [
-			'product' => new CatalogListResource($this->product),
+			'product' => new CartProductResource($this->product),
 			'quantity' => $this->quantity(),
 			'price' => $this->price(),
 			'total' => $this->total(),
