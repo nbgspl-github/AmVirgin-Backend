b8e5b796eee52e0cf3fa72ab7df43279cdfc59b2
Aviral Singh <mr.aviralsingh@gmail.com>
2020-04-23 02:25:08 +0530

    Updates

--- app/Resources/Orders/Seller/OrderResource.php
+++ app/Resources/Orders/Seller/OrderResource.php
@@ -9,7 +9,7 @@ class OrderResource extends JsonResource{
 		return [
 			'key' => $this->id(),
 			'customer' => new OrderCustomerResource($this->customer),
-			'name' => 'abc',
+			'name' => 'my hello',
 		];
 	}
 }
