9e8309bce471fdc126cb0793bc76e4d53e2a511d
Aviral Singh <mr.aviralsingh@gmail.com>
2020-04-23 02:10:59 +0530

    Updates

--- app/Resources/Orders/Seller/OrderCustomerResource.php
+++ app/Resources/Orders/Seller/OrderCustomerResource.php
@@ -6,15 +6,9 @@ use App\Classes\Time;
 
 class OrderCustomerResource extends \Illuminate\Http\Resources\Json\JsonResource{
 	public function toArray($request){
-		$status = $this->order;
-		$status = $status != null ? $status->status() : 'N/A';
 		return [
 			'key' => $this->id(),
 			'name' => $this->name(),
-			'orderNumber' => $this->orderNumber(),
-			'orderDate' => Time::mysqlStamp(strtotime($this->created_at)),
-			'status' => $status,
-			'quantity' => $this->items()->sum('quantity'),
 		];
 	}
 }
