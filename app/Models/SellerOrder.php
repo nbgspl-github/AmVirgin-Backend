aa9597d5f75c82930cc39629a0cdb5c1d8f1dc8e
Aviral Singh <dev.aviralx@gmail.com>
2020-04-23 02:26:52 +0530

    Updates

--- app/Models/SellerOrder.php
+++ app/Models/SellerOrder.php
@@ -21,12 +21,6 @@ class SellerOrder extends ModelExtended{
 		'orderId',
 		'orderNumber',
 	];
-	public const AllowedStatuses = [
-		ShipmentPlaced => [
-			ShipmentReadyForDispatch,
-			ShipmentDispatched,
-		],
-	];
 
 	public function seller(){
 		return $this->belongsTo('App\Models\Auth\Seller', 'sellerId');
