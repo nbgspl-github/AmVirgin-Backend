732cdd8216b37566e59cca370caeabaf6e9359bd
Aviral Singh <mr.aviralsingh@gmail.com>
2020-04-23 02:44:55 +0530

    Updates

--- app/Resources/Orders/Seller/OrderResource.php
+++ app/Resources/Orders/Seller/OrderResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Resources\Orders\Seller;
 
+use App\Models\SellerOrder;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class OrderResource extends JsonResource{
@@ -10,6 +11,7 @@ class OrderResource extends JsonResource{
 			'key' => $this->id(),
 			'customer' => new OrderCustomerResource($this->customer),
 			'items' => OrderItemResource::collection($this->items),
+			'transitions' => SellerOrder::AllowedStatuses[$this->status()],
 		];
 	}
 }
