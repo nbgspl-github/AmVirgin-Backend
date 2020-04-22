cbfa4b55eeac9492450151ec52debc09fd5d5321
Aviral Singh <mr.aviralsingh@gmail.com>
2020-04-23 02:00:31 +0530

    Updates

--- app/Classes/Eloquent/ModelExtended.php
+++ app/Classes/Eloquent/ModelExtended.php
@@ -6,6 +6,6 @@ use App\Classes\Str;
 
 class ModelExtended extends \Illuminate\Database\Eloquent\Model{
 	public function getTable(){
-		return $this->table ?? Str::camel(Str::pluralStudly(class_basename($this)));
+		return $this->table ?? Str::slug(Str::pluralStudly(class_basename($this)));
 	}
 }
