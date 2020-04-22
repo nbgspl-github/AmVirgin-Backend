9467a4cb84f14330c2e9dab2e5d934676d267761
Aviral Singh <mr.aviralsingh@gmail.com>
2020-04-23 02:40:52 +0530

    Updates

--- app/Models/SellerOrderItem.php
+++ app/Models/SellerOrderItem.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class SellerOrderItem extends Model{
 	use DynamicAttributeNamedMethods, RetrieveResource, RetrieveCollection;
+
 	protected $table = 'seller-order-items';
 	protected $fillable = [
 		'sellerOrderId',
@@ -20,7 +21,11 @@ class SellerOrderItem extends Model{
 		'updated_at',
 	];
 
-	public function productDetails() {
-		return $this->hasOne(Product::class, 'id','productId')->with('images');
+	public function productDetails(){
+		return $this->hasOne(Product::class, 'id', 'productId')->with('images');
+	}
+
+	public function product(){
+		return $this->hasOne(Product::class, 'productId');
 	}
 }
