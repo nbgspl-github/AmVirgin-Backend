78706370230799a770e7521226a85712a0943240
Aviral Singh <dev.aviralx@gmail.com>
2020-04-22 16:31:48 +0530

    Updates
    Signed-off-by: Aviral Singh <mr.aviralsingh@gmail.com>

--- app/Resources/Auth/Customer/AuthProfileResource.php
+++ app/Resources/Auth/Customer/AuthProfileResource.php
@@ -9,6 +9,7 @@ class AuthProfileResource extends JsonResource{
 
 	public function toArray($request){
 		return [
+			'key' => $this->id(),
 			'name' => $this->name(),
 			'email' => $this->email(),
 			'mobile' => $this->mobile(),
