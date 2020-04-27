6cb2d4326703eddecaad6442d0a8e5d19c5f1ed9
Aviral Singh <mr.aviralsingh@gmail.com>
2020-04-27 15:30:45 +0530

    Updates

--- app/Http/Controllers/App/Customer/AuthController.php
+++ app/Http/Controllers/App/Customer/AuthController.php
@@ -58,8 +58,8 @@ class AuthController extends BaseAuthController{
 
 	protected function rulesUpdatePassword(): array{
 		return [
-			'current' => ['bail', 'required', 'string', 'min4', 'max:64'],
-			'new' => ['bail', 'required', 'string', 'min4', 'max:64', 'different:current'],
+			'current' => ['bail', 'required', 'string', 'min:4', 'max:64'],
+			'new' => ['bail', 'required', 'string', 'min:4', 'max:64', 'different:current'],
 			'confirm' => ['bail', 'required', 'string', 'same:new'],
 		];
 	}
