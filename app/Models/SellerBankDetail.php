3acffd14441618d0600748950481a4959181452f
Aviral Singh <mr.aviralsingh@gmail.com>
2020-04-30 00:50:38 +0530

    Updates

--- app/Models/SellerBankDetail.php
+++ app/Models/SellerBankDetail.php
@@ -13,6 +13,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class SellerBankDetail extends ModelExtended{
 	use DynamicAttributeNamedMethods;
 
+	protected $fillable = ['*'];
 	protected $casts = [
 		'addressProofDocument' => 'uri',
 		'cancelledCheque' => 'uri',
