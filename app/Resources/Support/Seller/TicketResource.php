26cc4d9d0ead55bdb1ac3b3b0368c30de0800365
Aviral Singh <mr.aviralsingh@gmail.com>
2020-06-19 13:40:17 +0530

    Updates

--- app/Resources/Support/Seller/TicketResource.php
+++ app/Resources/Support/Seller/TicketResource.php
@@ -5,6 +5,7 @@ namespace App\Resources\Support\Seller;
 class TicketResource extends \Illuminate\Http\Resources\Json\JsonResource {
 	public function toArray ($request) {
 		return [
+			'key' => $this->id(),
 			'subject' => $this->subject(),
 			'description' => $this->description(),
 			'orderId' => $this->orderId(),
