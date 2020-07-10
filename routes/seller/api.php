61870e46da67f7d818a6c772d825d618264813ca
Aviral Singh <dev.aviralx@gmail.com>
2020-07-10 18:24:05 +0530

    Payment Overview API

--- routes/seller/api.php
+++ routes/seller/api.php
@@ -18,41 +18,41 @@ use App\Http\Controllers\App\Seller\TwoFactorAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix(Str::Empty)->group(static function () {
-	Route::get(Str::Empty, [TwoFactorAuthController::class, 'exists']);
-	Route::post('login', [TwoFactorAuthController::class, 'login']);
-	Route::post('register', [TwoFactorAuthController::class, 'register']);
-	Route::post('logout', [AuthController::class, 'logout'])->middleware(AuthSeller);
+    Route::get(Str::Empty, [TwoFactorAuthController::class, 'exists']);
+    Route::post('login', [TwoFactorAuthController::class, 'login']);
+    Route::post('register', [TwoFactorAuthController::class, 'register']);
+    Route::post('logout', [AuthController::class, 'logout'])->middleware(AuthSeller);
 
-	Route::prefix('profile')->group(static function () {
-		Route::get(Str::Empty, [AuthController::class, 'profile'])->middleware(AuthSeller);
-		Route::put(Str::Empty, [AuthController::class, 'updateProfile'])->middleware(AuthSeller);
-		Route::post('avatar', [AuthController::class, 'updateAvatar'])->middleware(AuthSeller);
-		Route::put('password', [AuthController::class, 'updatePassword'])->middleware(AuthSeller);
+    Route::prefix('profile')->group(static function () {
+        Route::get(Str::Empty, [AuthController::class, 'profile'])->middleware(AuthSeller);
+        Route::put(Str::Empty, [AuthController::class, 'updateProfile'])->middleware(AuthSeller);
+        Route::post('avatar', [AuthController::class, 'updateAvatar'])->middleware(AuthSeller);
+        Route::put('password', [AuthController::class, 'updatePassword'])->middleware(AuthSeller);
 
-		Route::prefix('business-details')->middleware(AuthSeller)->group(static function () {
-			Route::get(Str::Empty, [\App\Http\Controllers\App\Seller\BusinessDetailController::class, 'show']);
-			Route::post(Str::Empty, [\App\Http\Controllers\App\Seller\BusinessDetailController::class, 'update']);
-		});
+        Route::prefix('business-details')->middleware(AuthSeller)->group(static function () {
+            Route::get(Str::Empty, [\App\Http\Controllers\App\Seller\BusinessDetailController::class, 'show']);
+            Route::post(Str::Empty, [\App\Http\Controllers\App\Seller\BusinessDetailController::class, 'update']);
+        });
 
-		Route::prefix('bank-details')->middleware(AuthSeller)->group(static function () {
-			Route::get(Str::Empty, [\App\Http\Controllers\App\Seller\BankDetailController::class, 'show']);
-			Route::post(Str::Empty, [\App\Http\Controllers\App\Seller\BankDetailController::class, 'update']);
-		});
+        Route::prefix('bank-details')->middleware(AuthSeller)->group(static function () {
+            Route::get(Str::Empty, [\App\Http\Controllers\App\Seller\BankDetailController::class, 'show']);
+            Route::post(Str::Empty, [\App\Http\Controllers\App\Seller\BankDetailController::class, 'update']);
+        });
 
-		Route::prefix('contact-details')->group(static function () {
+        Route::prefix('contact-details')->group(static function () {
 
-		});
+        });
 
-		Route::prefix('pickup-details')->group(static function () {
+        Route::prefix('pickup-details')->group(static function () {
 
-		});
+        });
 
-		Route::prefix('mou')->group(static function () {
-			Route::get(Str::Empty, [\App\Http\Controllers\App\Seller\AgreementController::class, 'show']);
-			Route::get('status', [\App\Http\Controllers\App\Seller\AgreementController::class, 'index'])->middleware(AuthSeller);
-			Route::put(Str::Empty, [\App\Http\Controllers\App\Seller\AgreementController::class, 'update'])->middleware(AuthSeller);
-		});
-	});
+        Route::prefix('mou')->group(static function () {
+            Route::get(Str::Empty, [\App\Http\Controllers\App\Seller\AgreementController::class, 'show']);
+            Route::get('status', [\App\Http\Controllers\App\Seller\AgreementController::class, 'index'])->middleware(AuthSeller);
+            Route::put(Str::Empty, [\App\Http\Controllers\App\Seller\AgreementController::class, 'update'])->middleware(AuthSeller);
+        });
+    });
 });
 Route::post('change-password', [AuthController::class, 'changePassword'])->middleware(AuthSeller);
 Route::post('forgot-password', [AuthController::class, 'forgotPassword']);
@@ -61,80 +61,84 @@ Route::post('change-email-token', [AuthController::class, 'getChangeEmailToken']
 Route::post('reset-password-token', [AuthController::class, 'getResetPasswordToken']);
 
 Route::prefix('categories')->group(function () {
-	Route::get('/', [CategoryController::class, 'index'])->name('seller.categories.index');
-	Route::get('/{id}/attributes', [ListController::class, 'show'])->name('seller.categories.attributes.index');
+    Route::get('/', [CategoryController::class, 'index'])->name('seller.categories.index');
+    Route::get('/{id}/attributes', [ListController::class, 'show'])->name('seller.categories.attributes.index');
 });
 
 Route::prefix('attributes')->group(function () {
-	Route::get('/{attributeId}/values', [ValueController::class, 'show']);
+    Route::get('/{attributeId}/values', [ValueController::class, 'show']);
 });
 
 Route::middleware(AuthSeller)->prefix('products')->group(function () {
-	Route::get('/', [ProductController::class, 'index'])->name('seller.products.index');
-	Route::post(Str::Empty, [ProductController::class, 'store'])->name('seller.products.store');
-	Route::get('{id}', [ProductController::class, 'show'])->name('seller.products.show');
-	Route::get('edit/{id}', [ProductController::class, 'edit'])->name('seller.products.edit');
-	Route::post('{id}', [ProductController::class, 'update'])->name('seller.products.update');
-	Route::post('change-status/{id}', [ProductController::class, 'changeStatus'])->name('seller.products.change-status');
-	Route::delete('{id}', [ProductController::class, 'delete'])->name('seller.products.delete');
-	Route::delete('/images/{id}', [ProductImageController::class, 'delete'])->name('seller.products.images.delete');
-	Route::delete('/attributes/{id}', [ProductAttributeController::class, 'delete'])->name('seller.products.attributes.delete');
+    Route::get('/', [ProductController::class, 'index'])->name('seller.products.index');
+    Route::post(Str::Empty, [ProductController::class, 'store'])->name('seller.products.store');
+    Route::get('{id}', [ProductController::class, 'show'])->name('seller.products.show');
+    Route::get('edit/{id}', [ProductController::class, 'edit'])->name('seller.products.edit');
+    Route::post('{id}', [ProductController::class, 'update'])->name('seller.products.update');
+    Route::post('change-status/{id}', [ProductController::class, 'changeStatus'])->name('seller.products.change-status');
+    Route::delete('{id}', [ProductController::class, 'delete'])->name('seller.products.delete');
+    Route::delete('/images/{id}', [ProductImageController::class, 'delete'])->name('seller.products.images.delete');
+    Route::delete('/attributes/{id}', [ProductAttributeController::class, 'delete'])->name('seller.products.attributes.delete');
 
-	Route::prefix('token')->group(function () {
-		Route::get('create', [ProductController::class, 'token']);
-	});
+    Route::prefix('token')->group(function () {
+        Route::get('create', [ProductController::class, 'token']);
+    });
 
-	Route::prefix('trailer')->group(function () {
-		Route::post('upload', [\App\Http\Controllers\App\Seller\Products\ProductTrailerController::class, 'store']);
-	});
+    Route::prefix('trailer')->group(function () {
+        Route::post('upload', [\App\Http\Controllers\App\Seller\Products\ProductTrailerController::class, 'store']);
+    });
 });
 
 Route::prefix('currencies')->group(function () {
-	Route::get('/', [CurrencyController::class, 'index'])->name('seller.currencies.index');
+    Route::get('/', [CurrencyController::class, 'index'])->name('seller.currencies.index');
 });
 
 Route::prefix('countries')->group(function () {
-	Route::get('/', [CountryController::class, 'index'])->name('seller.countries.index');
-	Route::get('{countryId}/states', [StateController::class, 'index'])->name('seller.states.index');
-	Route::get('states/{stateId}/cities', [CityController::class, 'index'])->name('seller.states.index');
+    Route::get('/', [CountryController::class, 'index'])->name('seller.countries.index');
+    Route::get('{countryId}/states', [StateController::class, 'index'])->name('seller.states.index');
+    Route::get('states/{stateId}/cities', [CityController::class, 'index'])->name('seller.states.index');
 });
 
 Route::prefix('orders')->middleware('auth:seller-api')->group(function () {
-	Route::get(Str::Empty, [OrderController::class, 'index']);
-	Route::get('{id}', [OrderController::class, 'show']);
-	Route::get('download-pdf/{id}', [OrderController::class, 'orderDetails']);
-	Route::put('{id}', [OrderController::class, 'updateStatus']);
+    Route::get(Str::Empty, [OrderController::class, 'index']);
+    Route::get('{id}', [OrderController::class, 'show']);
+    Route::get('download-pdf/{id}', [OrderController::class, 'orderDetails']);
+    Route::put('{id}', [OrderController::class, 'updateStatus']);
 });
 Route::prefix('order')->middleware('auth:seller-api')->group(function () {
-	Route::get('/status', [OrderController::class, 'getOrderStatus']);
+    Route::get('/status', [OrderController::class, 'getOrderStatus']);
 });
 
 Route::prefix('orders-by-status')->middleware('auth:seller-api')->group(function () {
-	Route::get('/{param}', [OrderController::class, 'getOrderByStatus']);
+    Route::get('/{param}', [OrderController::class, 'getOrderByStatus']);
 });
 
 Route::prefix('customer')->middleware('auth:seller-api')->group(function () {
-	Route::get('{param}', [OrderController::class, 'customer']);
+    Route::get('{param}', [OrderController::class, 'customer']);
 });
 
 Route::prefix('hsn')->group(function () {
-	Route::get('/', [HsnCodeController::class, 'index']);
+    Route::get('/', [HsnCodeController::class, 'index']);
 });
 
 Route::prefix('brands')->middleware('auth:seller-api')->group(function () {
-	Route::get(Str::Empty, [\App\Http\Controllers\App\Seller\BrandController::class, 'index']);
-	Route::get('approved', [\App\Http\Controllers\App\Seller\BrandController::class, 'show']);
-	Route::post('approval', [\App\Http\Controllers\App\Seller\BrandController::class, 'store']);
+    Route::get(Str::Empty, [\App\Http\Controllers\App\Seller\BrandController::class, 'index']);
+    Route::get('approved', [\App\Http\Controllers\App\Seller\BrandController::class, 'show']);
+    Route::post('approval', [\App\Http\Controllers\App\Seller\BrandController::class, 'store']);
 });
 
 Route::prefix('announcements')->group(function () {
-	Route::get(Str::Empty, [\App\Http\Controllers\App\Seller\AnnouncementController::class, 'index'])->middleware(AuthSeller);
-	Route::put('{id}/mark', [\App\Http\Controllers\App\Seller\AnnouncementController::class, 'mark'])->middleware(AuthSeller);
+    Route::get(Str::Empty, [\App\Http\Controllers\App\Seller\AnnouncementController::class, 'index'])->middleware(AuthSeller);
+    Route::put('{id}/mark', [\App\Http\Controllers\App\Seller\AnnouncementController::class, 'mark'])->middleware(AuthSeller);
 });
 
 Route::prefix('support')->group(static function () {
-	Route::prefix('tickets')->group(static function () {
-		Route::get(Str::Empty, [\App\Http\Controllers\App\Seller\SupportController::class, 'index'])->middleware(AuthSeller);
-		Route::post(Str::Empty, [\App\Http\Controllers\App\Seller\SupportController::class, 'store'])->middleware(AuthSeller);
-	});
+    Route::prefix('tickets')->group(static function () {
+        Route::get(Str::Empty, [\App\Http\Controllers\App\Seller\SupportController::class, 'index'])->middleware(AuthSeller);
+        Route::post(Str::Empty, [\App\Http\Controllers\App\Seller\SupportController::class, 'store'])->middleware(AuthSeller);
+    });
+});
+
+Route::prefix('payments')->group(static function () {
+    Route::get('overview', [\App\Http\Controllers\App\Seller\Payments\OverviewController::class, 'show'])->middleware(AuthSeller);
 });
