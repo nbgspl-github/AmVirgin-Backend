e0f2d0368d3841147ca21412e89873ddc68be1c5
Dheerendra <ddpwpareshan@gmail.com>
2020-07-13 20:39:38 +0530

    Staus

--- app/Resources/Orders/Seller/OrderResource.php
+++ app/Resources/Orders/Seller/OrderResource.php
@@ -6,14 +6,12 @@ use App\Enums\Seller\OrderStatus;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class OrderResource extends JsonResource {
-	public function toArray ($request) {
-		$or = $this->order;
+	public function toArray ($request) { 
 		$status = $status != null ? $status->status() : OrderStatus::NotAvailable;
 		return [
 			'key' => $this->id(),
 			'orderId' => $this->orderNumber(),
-			'status' => $this->status(),
-			'or' => $or,
+			'status' => $this->status(), 
 			'customer' => new OrderCustomerResource($this->customer),
 			'items' => OrderItemResource::collection($this->items),
 			'transitions' => OrderStatus::transitions(new OrderStatus($status)),
