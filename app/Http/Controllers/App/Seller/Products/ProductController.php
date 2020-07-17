cc73997d60bf5849dfed96b8b23d7a10c7b2f6b9
Aviral Singh <dev.aviralx@gmail.com>
2020-07-17 10:53:04 +0530

    Updates

--- app/Http/Controllers/App/Seller/Products/ProductController.php
+++ app/Http/Controllers/App/Seller/Products/ProductController.php
@@ -1,44 +1,25 @@
 <?php
 
-namespace App\Http\Controllers\App\Seller;
+namespace App\Http\Controllers\App\Seller\Products;
 
-use Illuminate\Http\Request;
 use App\Classes\Arrays;
-use App\Classes\Rule;
 use App\Classes\Str;
-use App\Constants\OfferTypes;
-use App\Constants\WarrantyServiceType;
 use App\Exceptions\BrandNotApprovedForSellerException;
 use App\Exceptions\InvalidCategoryException;
 use App\Exceptions\TokenInvalidException;
 use App\Exceptions\ValidationException;
-use App\Http\Controllers\App\Seller\Products\AbstractProductController;
-use App\Http\Controllers\Web\ExtendedResourceController;
 use App\Interfaces\Directories;
-use App\Interfaces\Tables;
-use App\Models\Attribute;
-use App\Models\AttributeValue;
-use App\Models\Brand;
-use App\Models\Category;
 use App\Models\HsnCode;
 use App\Models\Product;
-use App\Models\ProductAttribute;
-use App\Models\ProductImage;
 use App\Models\ProductToken;
-use App\Queries\ProductQuery;
 use App\Resources\Products\Seller\CatalogListResource;
-use App\Resources\Products\Seller\ProductEditResource;
 use App\Resources\Products\Seller\ProductResource;
 use App\Storage\SecuredDisk;
-use App\Traits\ValidatesRequest;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Resources\ConditionallyLoadsAttributes;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 use Throwable;
 
 class ProductController extends AbstractProductController{
