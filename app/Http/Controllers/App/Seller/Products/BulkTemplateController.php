f10a35ac1f795ba662d2f32a7057672578aac0ad
Aviral Singh <dev.aviralx@gmail.com>
2020-07-18 15:04:16 +0530

    Updates

--- app/Http/Controllers/App/Seller/Products/BulkTemplateController.php
+++ app/Http/Controllers/App/Seller/Products/BulkTemplateController.php
@@ -6,13 +6,14 @@ namespace App\Http\Controllers\App\Seller\Products;
 
 use App\Exceptions\ValidationException;
 use App\Http\Controllers\Web\ExtendedResourceController;
+use App\Models\AttributeSetItem;
 use App\Models\Brand;
 use App\Models\Category;
 use App\Traits\ValidatesRequest;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Style\Color;
 use PhpOffice\PhpSpreadsheet\Writer\Xls;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
@@ -22,6 +23,8 @@ class BulkTemplateController extends ExtendedResourceController
 
     protected array $rules;
 
+    protected array $imageGuidelines;
+
     public function __construct()
     {
         parent::__construct();
@@ -30,6 +33,69 @@ class BulkTemplateController extends ExtendedResourceController
                 'categoryId' => 'bail|required|exists:categories,id',
                 'brandId' => 'bail|required|exists:brands,id'
             ]
+        ];
+        $this->imageGuidelines = [
+            [
+                "header" => true,
+                "text" => "Image criteria",
+            ],
+            [
+                "header" => false,
+                "text" => "All the images should have minimum image resolution of.",
+            ],
+            [
+                "header" => false,
+                "text" => "You must provide minimum 2 images for this category.",
+            ],
+            [
+                "header" => false,
+                "text" => "You can provide maximum 8 images for this category.",
+            ],
+            [
+                "header" => true,
+                "text" => "Image standards",
+            ],
+            [
+                "header" => false,
+                "text" => "Primary images should be in white background.",
+            ],
+            [
+                "header" => false,
+                "text" => "Images with grey background can be used for the white colored product.",
+            ],
+            [
+                "header" => false,
+                "text" => "Product should be displayed without packing.",
+            ],
+            [
+                "header" => false,
+                "text" => "Solo product image without any props.",
+            ],
+            [
+                "header" => false,
+                "text" => "Movie shots & scenes are not allowed. Celebrity face should not be morphed. However, if celebrity is brand ambassador then you can include celebrity images as image except primary image",
+            ],
+            [
+                "header" => false,
+                "text" => "The same model has to be used for all shots of a product in lifestyle image.",
+            ],
+            [
+                "header" => false,
+                "text" => "The product should be center aligned and cover a minimum of 90% of the frame.",
+            ],
+            [
+                "header" => true,
+                "text" => "Following Images will be rejected",
+            ],
+            [
+                "header" => false,
+                "text" => "Graphic/ Inverted/ Pixelated image are not accepted.",
+            ],
+            [
+                "header" => false,
+                "text" => "Images with text/Watermark are not acceptable in primary images.
+",
+            ],
         ];
     }
 
@@ -45,19 +111,65 @@ class BulkTemplateController extends ExtendedResourceController
             $validated = $this->requestValid(request(), $this->rules['show']);
             $category = Category::find($validated['categoryId']);
             $brand = Brand::find($validated['brandId']);
+            $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xls();
+            $images = $reader->load(public_path("static/Images.xls"));
             $spreadsheet = new Spreadsheet();
-            $worksheetA = new Worksheet($spreadsheet, 'First');
-            $worksheetA->setCodeName('First');
-            $worksheetA->setCellValue('A1', 'First Sheet Cell Value');
-            $worksheetB = new Worksheet($spreadsheet, 'Second');
-            $worksheetB->setCellValue('B1', 'Second Sheet Cell Value');
-            $worksheetA->setCodeName('Second');
-            $worksheetC = new Worksheet($spreadsheet, 'Third');
-            $worksheetC->setCellValue('C1', 'Third Sheet Cell Value');
-            $worksheetC->setCodeName('Third');
-            $worksheetD = new Worksheet($spreadsheet, 'Fourth');
-            $worksheetD->setCellValue('D1', 'Fourth Sheet Cell Value');
-            $worksheetD->setCodeName('Fourth');
+            $spreadsheet->removeSheetByIndex(0);
+            $worksheetSummary = $spreadsheet->createSheet();
+            $worksheetSummary->setTitle('Summary Sheet');
+            $worksheetIndex = $spreadsheet->createSheet();
+            $worksheetIndex->setTitle('Index');
+            $worksheetMain = $spreadsheet->createSheet();
+            $worksheetMain->setTitle($category->name);
+            $worksheetImages = $spreadsheet->createSheet();
+            $worksheetImages->setTitle('Image Guidelines');
+            $attributeSet = $category->attributeSet;
+            $index = 1;
+            $character = 65;
+            if ($attributeSet != null) {
+                $attributeSetItems = $attributeSet->items;
+                $attributeSetItems->each(function (AttributeSetItem $attributeSetItem) use (&$worksheetIndex, &$index, &$character, &$items, $category) {
+                    $attribute = $attributeSetItem->attribute;
+                    $richText = new \PhpOffice\PhpSpreadsheet\RichText\RichText();
+                    $payable = $richText->createTextRun($attribute->name);
+                    $payable->getFont()->setUnderline(true);
+                    $payable->getFont()->setColor(new \PhpOffice\PhpSpreadsheet\Style\Color(\PhpOffice\PhpSpreadsheet\Style\Color::COLOR_DARKBLUE));
+                    $worksheetIndex->setCellValue(sprintf('%c%d', $character, $index), $richText);
+                    $worksheetIndex->getCell(sprintf('%c%d', $character, $index))->getHyperlink()->setUrl(sprintf("sheet://'%s' !A1", $category->name));
+                    $worksheetIndex->getCell(sprintf('%c%d', $character, $index))->getStyle()->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+                    $worksheetIndex->getColumnDimension(sprintf('%c', $character))->setAutoSize(true);
+                    $currentIndex = 2;
+                    foreach ($attribute->values() as $value) {
+                        $worksheetIndex->setCellValue(sprintf('%c%d', $character, $currentIndex), $value);
+                        $worksheetIndex->getCell(sprintf('%c%d', $character, $currentIndex))->getStyle()->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+                        $currentIndex++;
+                    }
+                    $character++;
+                });
+            } else {
+                $worksheetIndex->setCellValue('A1', 'No attributes found!');
+            }
+            $index = 1;
+            $character = 65;
+            $worksheetImages->getColumnDimension('A')->setAutoSize(true);
+            foreach ($this->imageGuidelines as $guideline) {
+                $item = $guideline;
+                $cell = sprintf('%c%d', $character, $index);
+                if ($item['header'] == true) {
+                    if ($index != 2)
+                        $worksheetImages->setCellValue($cell, '');
+                    $index++;
+                    $cell = sprintf('%c%d', $character, $index);
+                    $worksheetImages->setCellValue($cell, $item['text']);
+                    $worksheetImages->getCell($cell)->getStyle()->getFont()->setBold(true)->setColor(new Color(Color::COLOR_BLACK));
+                    $worksheetImages->getCell($cell)->getStyle()->getFill()
+                        ->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
+                        ->getStartColor()->setRGB('E0E0E0');
+                } else {
+                    $worksheetImages->setCellValue($cell, $item['text']);
+                }
+                $index++;
+            }
             $writer = new Xls($spreadsheet);
             $response = new StreamedResponse(
                 function () use ($writer) {
