0e6ec3f7415113e287d3d4cb1cbdbd2902b29ed9
Aviral Singh <dev.aviralx@gmail.com>
2020-07-28 13:12:43 +0530

    Updates

--- app/Models/SellerBusinessDetail.php
+++ app/Models/SellerBusinessDetail.php
@@ -2,78 +2,99 @@
 
 namespace App\Models;
 
-use App\Classes\Arrays;
 use App\Classes\Eloquent\ModelExtended;
-use App\Classes\Rule;
 use App\Interfaces\Directories;
-use App\Interfaces\Tables;
-use App\Queries\Seller\BankDetailQuery;
 use App\Queries\Seller\BusinessDetailQuery;
 use App\Storage\SecuredDisk;
 use App\Traits\DynamicAttributeNamedMethods;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Http\UploadedFile;
 
-class SellerBusinessDetail extends ModelExtended{
-	use DynamicAttributeNamedMethods;
+class SellerBusinessDetail extends ModelExtended
+{
+    use DynamicAttributeNamedMethods;
 
-	protected $fillable = [
-		'sellerId',
-		'name',
-		'nameVerified',
-		'tan',
-		'gstIN',
-		'gstINVerified',
-		'signature',
-		'signatureVerified',
-		'rbaFirstLine',
-		'rbaSecondLine',
-		'rbaPinCode',
-		'rbaCityId',
-		'rbaStateId',
-		'rbaCountryId',
-		'pan',
-		'panVerified',
-		'panProofDocument',
-		'addressProofType',
-		'addressProofDocument',
-	];
-	protected $casts = [
-		'signature' => 'uri',
-		'addressProofDocument' => 'uri',
-		'signatureVerified' => 'bool',
-		'gstINVerified' => 'bool',
-		'panVerified' => 'bool',
-		'panProofDocument' => 'uri',
-		'nameVerified' => 'bool',
-	];
+    protected $fillable = [
+        'sellerId',
+        'name',
+        'nameVerified',
+        'tan',
+        'gstIN',
+        'gstCertificate',
+        'gstINVerified',
+        'signature',
+        'signatureVerified',
+        'rbaFirstLine',
+        'rbaSecondLine',
+        'rbaPinCode',
+        'rbaCityId',
+        'rbaStateId',
+        'rbaCountryId',
+        'pan',
+        'panVerified',
+        'panProofDocument',
+        'addressProofType',
+        'addressProofDocument',
+    ];
+    protected $casts = [
+        'signature' => 'uri',
+        'addressProofDocument' => 'uri',
+        'signatureVerified' => 'bool',
+        'gstINVerified' => 'bool',
+        'panVerified' => 'bool',
+        'panProofDocument' => 'uri',
+        'nameVerified' => 'bool',
+    ];
 
-	public function setSignatureAttribute($value){
-		$this->attributes['signature'] = SecuredDisk::access()->putFile(Directories::SellerDocuments, $value);
-		return $this->attributes['signature'];
-	}
-	public function setPanProofDocumentAttribute($value){
-		$this->attributes['panProofDocument'] = SecuredDisk::access()->putFile(Directories::SellerDocuments, $value);
-		return $this->attributes['panProofDocument'];
-	}
+    public function setSignatureAttribute($value)
+    {
+        $this->attributes['signature'] = SecuredDisk::access()->putFile(Directories::SellerDocuments, $value);
+        return $this->attributes['signature'];
+    }
 
-	public function setAddressProofDocumentAttribute($value){
-		$this->attributes['addressProofDocument'] = SecuredDisk::access()->putFile(Directories::SellerDocuments, $value);
-		return $this->attributes['addressProofDocument'];
-	}
-	public function state(): BelongsTo{
-		return $this->belongsTo(State::class, 'rbaStateId');
-	}
+    public function setPanProofDocumentAttribute($value)
+    {
+        $this->attributes['panProofDocument'] = SecuredDisk::access()->putFile(Directories::SellerDocuments, $value);
+        return $this->attributes['panProofDocument'];
+    }
 
-	public function city(): BelongsTo{
-		return $this->belongsTo(City::class, 'rbaCityId');
-	}
+    public function setAddressProofDocumentAttribute($value)
+    {
+        $this->attributes['addressProofDocument'] = SecuredDisk::access()->putFile(Directories::SellerDocuments, $value);
+        return $this->attributes['addressProofDocument'];
+    }
 
-	public function country(){
-		return Country::where('initials', 'IN')->first();
-	}
+    public function setGstCertificateAttribute($value)
+    {
+        if ($value instanceof UploadedFile)
+            $this->attributes['gstCertificate'] = SecuredDisk::access()->putFile(Directories::SellerDocuments, $value);
+        else
+            $this->attributes['gstCertificate'] = $value;
+        return $this->attributes['gstCertificate'];
+    }
 
-	public static function startQuery(): BusinessDetailQuery{
-		return BusinessDetailQuery::begin();
-	}
+    public function getGstCertificateAttribute($value)
+    {
+        return SecuredDisk::existsUrl($this->attributes['gstCertificate']);
+    }
+
+    public function state(): BelongsTo
+    {
+        return $this->belongsTo(State::class, 'rbaStateId');
+    }
+
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class, 'rbaCityId');
+    }
+
+    public function country()
+    {
+        return Country::where('initials', 'IN')->first();
+    }
+
+    public static function startQuery(): BusinessDetailQuery
+    {
+        return BusinessDetailQuery::begin();
+    }
 }
