68b8ce4731c223d972a41589f9030a9d1e534a66
Aviral Singh <dev.aviralx@gmail.com>
2020-12-17 16:55:46 +0530

    Updates

--- app/Http/Controllers/Api/Seller/Orders/Status/Handlers/Cancelled.php
+++ app/Http/Controllers/Api/Seller/Orders/Status/Handlers/Cancelled.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Seller\Orders\Status\Handlers;
 use App\Classes\Builders\ResponseBuilder;
 use App\Http\Controllers\Api\Seller\Orders\Status\Contracts\Action;
 use App\Library\Enums\Orders\Status;
+use App\Library\Utils\Extensions\Time;
 use App\Models\Auth\Seller;
 use App\Models\SubOrder;
 use Illuminate\Http\Response;
@@ -20,12 +21,28 @@ class Cancelled implements Action
 
 	public function allowed (SubOrder $order, Status $action, Status $next) : bool
 	{
-		return $action->isNot($next);
+		return (
+			$action->isNot($next)
+			&&
+			!($order->status->is(Status::Cancelled) || $order->status->is(Status::Delivered))
+			&&
+			empty($order->fulfilled_at)
+		);
 	}
 
 	public function handle (SubOrder $order, Status $next, array $extra = []) : ResponseBuilder
 	{
-		$order->update(array_merge(['status' => $next->value], $extra));
+		$order->update([
+			'status' => Status::Cancelled,
+			'cancelled_at' => Time::mysqlStamp()
+		]);
+		$main = $order->order;
+		if ($main != null && ($main->subOrders()->where('status', Status::Cancelled)->count() == $main->subOrders()->count())) {
+			$main->update([
+				'status' => Status::Cancelled,
+				'cancelled_at' => Time::mysqlStamp()
+			]);
+		}
 		return responseApp()->status(Response::HTTP_OK)->message('Action performed successfully!');
 	}
 
