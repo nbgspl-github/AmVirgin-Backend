7a922da90f731088f077a6d39a1310a022d2a8b2
Aviral Singh <dev.aviralx@gmail.com>
2021-01-10 00:14:16 +0530

    Updates
    Signed-off-by: Aviral Singh <dev.aviralx@gmail.com>

--- app/Jobs/TranscoderTask.php
+++ app/Jobs/TranscoderTask.php
@@ -2,12 +2,6 @@
 
 namespace App\Jobs;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
-
 class TranscoderTask
 {
 //	use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
@@ -30,9 +24,9 @@ class TranscoderTask
 		$this->source = $source;
 		$this->path = $this->source->getRawOriginal('file');
 		$this->formats = [
-			\App\Library\Enums\Videos\Quality::SD => (new \FFMpeg\Format\Video\X264())->setKiloBitrate(1200),
-			\App\Library\Enums\Videos\Quality::HD => (new \FFMpeg\Format\Video\X264())->setKiloBitrate(2500),
-			\App\Library\Enums\Videos\Quality::FHD => (new \FFMpeg\Format\Video\X264())->setKiloBitrate(5000),
+			\App\Library\Enums\Videos\Quality::SD => (new \FFMpeg\Format\Video\X264('aac'))->setKiloBitrate(1200),
+			\App\Library\Enums\Videos\Quality::HD => (new \FFMpeg\Format\Video\X264('aac'))->setKiloBitrate(2500),
+			\App\Library\Enums\Videos\Quality::FHD => (new \FFMpeg\Format\Video\X264('aac'))->setKiloBitrate(5000),
 		];
 	}
 
