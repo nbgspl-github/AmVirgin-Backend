5822cec0daaeb4d5be0d056e83b4f3b5763a8344
Aviral Singh <dev.aviralx@gmail.com>
2021-01-29 19:06:11 +0530

    Updates
    Signed-off-by: Aviral Singh <dev.aviralx@gmail.com>

--- app/Models/Models/SellerTransaction.php
+++ app/Models/Models/SellerTransaction.php
@@ -10,4 +10,9 @@ class SellerTransaction extends \App\Library\Database\Eloquent\Model
 	{
 		return $this->hasMany(\App\Models\SellerPayment::class, 'transaction_id');
 	}
+
+	public function seller () : \Illuminate\Database\Eloquent\Relations\BelongsTo
+	{
+		return $this->belongsTo(\App\Models\Auth\Seller::class, 'seller_id');
+	}
 }
