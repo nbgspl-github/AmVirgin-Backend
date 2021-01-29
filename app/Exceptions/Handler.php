4a13b3edfaff494a7c7eb57e0a9fd7a01f059d38
Aviral Singh <dev.aviralx@gmail.com>
2021-01-29 12:59:00 +0530

    Updates
    Signed-off-by: Aviral Singh <dev.aviralx@gmail.com>

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -42,7 +42,6 @@ class Handler extends ExceptionHandler
 
 	public function render ($request, Throwable $e)
 	{
-		dd($e);
 		try {
 			if ($this->respondWithJson($request)) {
 				if ($e instanceof ModelNotFoundException || $e instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
