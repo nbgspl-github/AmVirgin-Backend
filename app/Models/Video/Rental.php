0f23275b6c1386d7e75120bbae420dd4e0f0121f
Aviral Singh <dev.aviralx@gmail.com>
2021-02-17 15:53:30 +0530

    Updates
    Signed-off-by: Aviral Singh <dev.aviralx@gmail.com>

--- app/Models/Video/Rental.php
+++ app/Models/Video/Rental.php
@@ -5,4 +5,9 @@ namespace App\Models\Video;
 class Rental extends \App\Library\Database\Eloquent\Model
 {
 	protected $table = 'rentals';
+
+	public function video () : \Illuminate\Database\Eloquent\Relations\BelongsTo
+	{
+		return $this->belongsTo(Video::class, 'video_id');
+	}
 }
