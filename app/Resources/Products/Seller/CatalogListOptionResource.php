c43049f83050bc9d43ca766bc834db55871c35f7
younggeeksin <raju@younggeeks.in>
2021-02-25 12:55:28 +0530

    Updates

--- app/Resources/Products/Seller/CatalogListOptionResource.php
+++ app/Resources/Products/Seller/CatalogListOptionResource.php
@@ -4,11 +4,13 @@ namespace App\Resources\Products\Seller;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class CatalogListOptionResource extends JsonResource{
-	public function toArray($request){
-		return [
-			'label' => $this->label(),
-			'value' => $this->value,
-		];
-	}
+class CatalogListOptionResource extends JsonResource
+{
+    public function toArray ($request): array
+    {
+        return [
+            'label' => $this->label,
+            'value' => $this->value,
+        ];
+    }
 }
--- app/Resources/Products/Seller/CatalogListResource.php
+++ app/Resources/Products/Seller/CatalogListResource.php
@@ -9,24 +9,24 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class CatalogListResource extends JsonResource
 {
-	public function toArray ($request)
-	{
-		return [
-			'key' => $this->id,
-			'name' => $this->name,
-			'slug' => $this->slug,
-			'category' => $this->category->name,
-			'sku' => $this->sku,
-			'stock' => $this->stock,
-			'listingStatus' => $this->listingStatus,
-			'listingDate' => Time::mysqlStamp(strtotime($this->created_at)),
-			'type' => $this->type,
-			'idealFor' => $this->idealFor,
-			'originalPrice' => $this->originalPrice,
-			'sellingPrice' => $this->sellingPrice,
-			'image' => Uploads::existsUrl($this->primaryImage),
-			'options' => CatalogListOptionResource::collection($this->options()->where('showInCatalogListing', true)->get()),
-			'variants' => $this->when($this->type == Product::Type['Variant'], VariantItemResource::collection($this->variants)),
-		];
-	}
+    public function toArray ($request)
+    {
+        return [
+            'key' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'category' => $this->category->name,
+            'sku' => $this->sku,
+            'stock' => $this->stock,
+            'listingStatus' => $this->listingStatus,
+            'listingDate' => Time::mysqlStamp(strtotime($this->created_at)),
+            'type' => $this->type,
+            'idealFor' => $this->idealFor,
+            'originalPrice' => $this->originalPrice,
+            'sellingPrice' => $this->sellingPrice,
+            'image' => $this->primaryImage,
+            'options' => CatalogListOptionResource::collection($this->options()->where('showInCatalogListing', true)->get()),
+            'variants' => $this->when($this->type == Product::Type['Variant'], VariantItemResource::collection($this->variants)),
+        ];
+    }
 }
