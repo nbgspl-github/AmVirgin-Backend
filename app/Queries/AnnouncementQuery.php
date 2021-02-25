71541dbc327a9d01bbccd67883da76f31c2fecec
younggeeksin <raju@younggeeks.in>
2021-02-25 15:56:33 +0530

    Updates

--- app/Queries/AnnouncementQuery.php
+++ app/Queries/AnnouncementQuery.php
@@ -2,35 +2,43 @@
 
 namespace App\Queries;
 
+use App\Library\Utils\Extensions\Time;
 use App\Models\Announcement;
 use Illuminate\Support\Carbon;
 
-class AnnouncementQuery extends AbstractQuery{
-	protected function model(): string{
-		return Announcement::class;
-	}
+class AnnouncementQuery extends AbstractQuery
+{
+    protected function model (): string
+    {
+        return Announcement::class;
+    }
 
-	public static function begin(): self{
-		return new self();
-	}
+    public static function begin (): self
+    {
+        return new self();
+    }
 
-	public function displayable(): self{
-		$current = Carbon::now()->toDateTimeString();
-		$this->query->where('validFrom', '<=', $current)->where('validUntil', '>=', $current);
-		return $this;
-	}
+    public function displayable (): self
+    {
+        $current = now()->format(Time::MYSQL_FORMAT);
+        $this->query->where('validFrom', '<=', $current)->where('validUntil', '>=', $current);
+        return $this;
+    }
 
-	public function excludeDeleted(): self{
-		$this->query->whereJsonDoesntContain('deletedBy', $this->user()->id());
-		return $this;
-	}
+    public function excludeDeleted (): self
+    {
+        $this->query->whereJsonDoesntContain('deletedBy', $this->user()->id);
+        return $this;
+    }
 
-	public function excludeRead(): self{
-		$this->query->whereJsonDoesntContain('readBy', $this->user()->id());
-		return $this;
-	}
+    public function excludeRead (): self
+    {
+        $this->query->whereJsonDoesntContain('readBy', $this->user()->id);
+        return $this;
+    }
 
-	protected function user(){
-		return auth('seller-api')->user();
-	}
+    protected function user ()
+    {
+        return auth('seller-api')->user();
+    }
 }
