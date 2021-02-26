d83636b05b258c58a8a8084817413be5ac0d750b
younggeeksin <raju@younggeeks.in>
2021-02-26 13:39:05 +0530

    Updates

--- app/Http/Modules/Admin/Controllers/Web/Users/SellerController.php
+++ app/Http/Modules/Admin/Controllers/Web/Users/SellerController.php
@@ -5,72 +5,74 @@ namespace App\Http\Modules\Admin\Controllers\Web\Users;
 use App\Http\Modules\Admin\Requests\Users\Seller\StoreRequest;
 use App\Http\Modules\Admin\Requests\Users\Seller\UpdateRequest;
 use App\Models\Auth\Seller;
+use Exception;
+use Illuminate\Http\JsonResponse;
 
 class SellerController extends \App\Http\Modules\Admin\Controllers\Web\WebController
 {
-	/**
-	 * @var Seller
-	 */
-	protected $model;
+    /**
+     * @var Seller
+     */
+    protected $model;
 
-	public function __construct ()
-	{
-		parent::__construct();
-		$this->middleware(AUTH_ADMIN);
-		$this->model = app(Seller::class);
-	}
+    public function __construct ()
+    {
+        parent::__construct();
+        $this->middleware(AUTH_ADMIN);
+        $this->model = app(Seller::class);
+    }
 
-	public function index ()
-	{
-		return view('admin.sellers.index')->with('users',
-			$this->paginateWithQuery(
-				$this->model->newQuery()->latest()->whereLike('name', $this->queryParameter()))
-		);
-	}
+    public function index ()
+    {
+        return view('admin.sellers.index')->with('users',
+            $this->paginateWithQuery(
+                $this->model->newQuery()->latest()->whereLike('name', $this->queryParameter()))
+        );
+    }
 
-	public function create ()
-	{
-		return view('admin.sellers.create');
-	}
+    public function create ()
+    {
+        return view('admin.sellers.create');
+    }
 
-	public function edit (Seller $seller)
-	{
-		return view('admin.sellers.edit')->with('seller', $seller);
-	}
+    public function edit (Seller $seller)
+    {
+        return view('admin.sellers.edit')->with('seller', $seller);
+    }
 
-	public function show (Seller $seller) : \Illuminate\Http\JsonResponse
-	{
-		return response()->json(
-			view('admin.sellers.show')->with('seller', $seller)->render()
-		);
-	}
+    public function show (Seller $seller): JsonResponse
+    {
+        return response()->json(
+            view('admin.sellers.show')->with('seller', $seller)->render()
+        );
+    }
 
-	public function store (StoreRequest $request) : \Illuminate\Http\RedirectResponse
-	{
-		$this->model->create($request->validated());
-		return redirect()->route(
-			'admin.sellers.index'
-		);
-	}
+    public function store (StoreRequest $request): \Illuminate\Http\RedirectResponse
+    {
+        $this->model->create($request->validated());
+        return redirect()->route(
+            'admin.sellers.index'
+        );
+    }
 
-	public function update (UpdateRequest $request, Seller $seller) : \Illuminate\Http\RedirectResponse
-	{
-		$seller->update($request->validated());
-		return redirect()->route(
-			'admin.sellers.index'
-		)->with('success', 'Seller details updated successfully.');
-	}
+    public function update (UpdateRequest $request, Seller $seller): \Illuminate\Http\RedirectResponse
+    {
+        $seller->update($request->validated());
+        return redirect()->route(
+            'admin.sellers.index'
+        )->with('success', 'Seller details updated successfully.');
+    }
 
-	/**
-	 * @param Seller $seller
-	 * @return \Illuminate\Http\JsonResponse
-	 * @throws \Exception
-	 */
-	public function delete (Seller $seller) : \Illuminate\Http\JsonResponse
-	{
-		$seller->delete();
-		return responseApp()->prepare(
-			null, \Illuminate\Http\Response::HTTP_OK, 'Seller deleted successfully.'
-		);
-	}
+    /**
+     * @param Seller $seller
+     * @return JsonResponse
+     * @throws Exception
+     */
+    public function delete (Seller $seller): JsonResponse
+    {
+        $seller->delete();
+        return responseApp()->prepare(
+            null, \Illuminate\Http\Response::HTTP_OK, 'Seller deleted successfully.'
+        );
+    }
 }
