9798f09ac6bab06439be9fa5f51d1bdb088dc3a4
younggeeksin <raju@younggeeks.in>
2021-03-01 13:41:04 +0530

    Updates

--- app/Http/Modules/Customer/Controllers/Api/News/Categories/CategoryController.php
+++ app/Http/Modules/Customer/Controllers/Api/News/Categories/CategoryController.php
@@ -7,22 +7,24 @@ use Illuminate\Http\JsonResponse;
 
 class CategoryController extends \App\Http\Modules\Customer\Controllers\Api\ApiController
 {
-	public function index () : JsonResponse
-	{
-		return responseApp()->prepare([
-			'categories' => \App\Resources\News\Category\Customer\ListResource::collection(Category::all()),
-			'articles' => \App\Resources\News\Articles\ArticleCollection::collection(
-				\App\Models\News\Article::query()->latest()->orderByDesc('views')->paginate($this->paginationChunk())
-			)->response()->getData()
-		]);
-	}
+    public function index (): JsonResponse
+    {
+        return responseApp()->prepare([
+            'categories' => \App\Resources\News\Category\Customer\ListResource::collection(
+                Category::query()->orderBy('order')->get()
+            ),
+            'articles' => \App\Resources\News\Articles\ArticleCollection::collection(
+                \App\Models\News\Article::query()->latest()->orderByDesc('views')->paginate($this->paginationChunk())
+            )->response()->getData()
+        ]);
+    }
 
-	public function show (Category $category) : JsonResponse
-	{
-		return responseApp()->prepare(
-			\App\Resources\News\Articles\ArticleCollection::collection(
-				$category->items()->latest('published_at')->paginate($this->paginationChunk())
-			)->response()->getData()
-		);
-	}
+    public function show (Category $category): JsonResponse
+    {
+        return responseApp()->prepare(
+            \App\Resources\News\Articles\ArticleCollection::collection(
+                $category->items()->latest('published_at')->paginate($this->paginationChunk())
+            )->response()->getData()
+        );
+    }
 }
