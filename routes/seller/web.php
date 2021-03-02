086a4658150179d4d9a5d010252e7e3bf9934fa3
younggeeksin <raju@younggeeks.in>
2021-03-02 16:09:47 +0530

    Updates

--- routes/seller/web.php
+++ routes/seller/web.php
@@ -1,4 +1,7 @@
 <?php
+
+use App\Http\Modules\Seller\Controllers\Web\WebLinksController;
+
 \Illuminate\Support\Facades\Route::get('privacy-policy', [WebLinksController::class, 'privacyPolicy']);
 \Illuminate\Support\Facades\Route::get('about-us', [WebLinksController::class, 'aboutUs']);
 \Illuminate\Support\Facades\Route::get('terms-conditions', [WebLinksController::class, 'termsAndConditions']);
