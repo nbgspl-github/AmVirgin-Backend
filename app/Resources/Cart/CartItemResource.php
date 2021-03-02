ce02390441f02e33e3205ddb03a44b595b8e5f98
younggeeksin <raju@younggeeks.in>
2021-03-02 18:43:02 +0530

    Updates

--- app/Resources/Cart/CartItemResource.php
+++ app/Resources/Cart/CartItemResource.php
@@ -6,21 +6,19 @@ use App\Library\Utils\Uploads;
 use App\Resources\Products\Customer\OptionResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class CartItemResource extends JsonResource{
-	public static function withoutWrapping(){
-		return true;
-	}
-
-	public function toArray($request){
-		return [
-			'name' => $this->name(),
-			'price' => [
-				'original' => $this->originalPrice(),
-				'selling' => $this->sellingPrice(),
-			],
-			'maxAllowedQuantity' => $this->maxQuantityPerOrder(),
-			'image' => Uploads::existsUrl($this->primaryImage()),
-			'options' => OptionResource::collection($this->options),
-		];
-	}
+class CartItemResource extends JsonResource
+{
+    public function toArray ($request): array
+    {
+        return [
+            'name' => $this->name,
+            'price' => [
+                'original' => $this->originalPrice,
+                'selling' => $this->sellingPrice,
+            ],
+            'maxAllowedQuantity' => $this->maxQuantityPerOrder,
+            'image' => $this->primaryImage,
+            'options' => OptionResource::collection($this->options),
+        ];
+    }
 }
--- app/Resources/Products/Customer/OptionResource.php
+++ app/Resources/Products/Customer/OptionResource.php
@@ -4,13 +4,15 @@ namespace App\Resources\Products\Customer;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class OptionResource extends JsonResource{
-	public function toArray($request){
-		return [
-			'label' => $this->label(),
-			'interface' => $this->interface(),
-			'group' => $this->group(),
-			'value' => $this->value(),
-		];
-	}
+class OptionResource extends JsonResource
+{
+    public function toArray ($request): array
+    {
+        return [
+            'label' => $this->label,
+            'interface' => $this->interface,
+            'group' => $this->group,
+            'value' => $this->value,
+        ];
+    }
 }
