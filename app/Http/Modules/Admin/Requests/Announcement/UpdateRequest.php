7a1df9921e6c7f7998cadea69d2f889c995fdc67
younggeeksin <raju@younggeeks.in>
2021-03-04 18:07:44 +0530

    Updates

--- app/Http/Modules/Admin/Requests/Announcement/UpdateRequest.php
+++ app/Http/Modules/Admin/Requests/Announcement/UpdateRequest.php
@@ -9,7 +9,7 @@ class UpdateRequest extends \Illuminate\Foundation\Http\FormRequest
         return [
             'title' => 'bail|required|string|max:255',
             'content' => 'bail|required|string|max:5000',
-            'banner' => 'bail|required|image|max:2048',
+            'banner' => 'bail|nullable|image|max:2048',
             'validFrom' => 'bail|required|date',
             'validUntil' => 'bail|required|date|after:validFrom',
         ];
