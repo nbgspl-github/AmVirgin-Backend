40524faf08af2fde355ebc289c30a9ddf3f8100b
younggeeksin <raju@younggeeks.in>
2021-03-04 18:47:07 +0530

    Updates

--- app/Queries/VideoQuery.php
+++ app/Queries/VideoQuery.php
@@ -11,118 +11,118 @@ use Illuminate\Database\Eloquent\Builder;
 class VideoQuery extends AbstractQuery
 {
 
-	protected function model(): string
-	{
-		return Video::class;
-	}
+    protected function model (): string
+    {
+        return Video::class;
+    }
 
-	public static function begin(): self
-	{
-		return new self();
-	}
+    public static function begin (): self
+    {
+        return new self();
+    }
 
-	public function displayable(): self
-	{
-		$this->query->where('pending', false);
-		return $this;
-	}
+    public function displayable (): self
+    {
+        $this->query->where('pending', false);
+        return $this;
+    }
 
-	public function isNotTranscoding(): self
-	{
-		$this->query->whereHas('queues', function (Builder $builder) {
-			$builder->whereNotIn('status', ['Encoding', 'Queued']);
-		})->orWhereDoesntHave('queues');
-		return $this;
-	}
+    public function isNotTranscoding (): self
+    {
+        $this->query->whereHas('queues', function (Builder $builder) {
+            $builder->whereNotIn('status', ['Encoding', 'Queued']);
+        })->orWhereDoesntHave('queues');
+        return $this;
+    }
 
-	public function trending(bool $yes = true): self
-	{
-		$this->query->orderByDesc('hits');
-		return $this;
-	}
+    public function trending (bool $yes = true): self
+    {
+        $this->query->orderByDesc('hits');
+        return $this;
+    }
 
-	public function genre($genreId = null): self
-	{
-		if ($genreId != null)
-			$this->query->where('genre_id', $genreId);
-		return $this;
-	}
+    public function genre ($genreId = null): self
+    {
+        if ($genreId != null)
+            $this->query->where('genre_id', $genreId);
+        return $this;
+    }
 
-	public function section(int $sectionId): self
-	{
-		$this->query->whereJsonContains('sections', $sectionId);
-		return $this;
-	}
+    public function section (int $sectionId): self
+    {
+        $this->query->whereJsonContains('sections', $sectionId);
+        return $this;
+    }
 
-	public function movie(): self
-	{
-		$this->query->where('type', 'movie');
-		return $this;
-	}
+    public function movie (): self
+    {
+        $this->query->where('type', 'movie');
+        return $this;
+    }
 
-	public function series(): self
-	{
-		$this->query->where('type', 'series');
-		return $this;
-	}
+    public function series (): self
+    {
+        $this->query->where('type', 'series');
+        return $this;
+    }
 
-	public function isNew(): self
-	{
-		$lastWeek = Time::mysqlStamp(time() - 604800);
-		$this->query->where('created_at', '>=', $lastWeek);
-		return $this;
-	}
+    public function isNew (): self
+    {
+        $lastWeek = Time::mysqlStamp(time() - 604800);
+        $this->query->where('created_at', '>=', $lastWeek);
+        return $this;
+    }
 
-	public function language($languageId = null): self
-	{
-		$language = Language::find($languageId);
-		if ($language != null) {
-			$language = $language->name;
-			$this->query->where('languageSlug', 'LIKE', "%{$language}%");
-		}
-		return $this;
-	}
+    public function language ($languageId = null): self
+    {
+        $language = Language::find($languageId);
+        if ($language != null) {
+            $language = $language->name;
+            $this->query->where('languageSlug', 'LIKE', "%{$language}%");
+        }
+        return $this;
+    }
 
-	public function includeExplicit($include = false): self
-	{
-		if ($include == true) {
-			$this->query->whereIn('pg_rating', ['G', 'PG', 'PG-13', 'R', 'NC-17']);
-		} else {
-			$this->query->whereIn('pg_rating', ['G', 'PG', 'PG-13', 'R']);
-		}
-		return $this;
-	}
+    public function includeExplicit ($include = false): self
+    {
+        if ($include == true) {
+            $this->query->whereIn('pg_rating', ['G', 'PG', 'PG-13', 'R', 'NC-17']);
+        } else {
+            $this->query->whereIn('pg_rating', ['G', 'PG', 'PG-13', 'R']);
+        }
+        return $this;
+    }
 
-	public function subscriptionType($subscriptionType = null): self
-	{
-		if ($subscriptionType != null) {
-			$this->query->where('subscription_type', $subscriptionType);
-		}
-		return $this;
-	}
+    public function subscriptionType ($subscriptionType = null): self
+    {
+        if ($subscriptionType != null) {
+            $this->query->where('subscription_type', $subscriptionType);
+        }
+        return $this;
+    }
 
-	public function applyFilters(bool $apply = true): self
-	{
-		if ($apply) {
-			$languages = Str::split(',', request('language'), true);
-			if (count($languages) > 0) {
-				$this->query->whereHas('audios', function (Builder $q) use ($languages) {
-					$q->whereIn('video_language_id', $languages);
-				});
-			}
-			$genres = Str::split(',', request('genre'), true);
-			if (count($genres) > 0) {
-				$this->query->whereIn('genre_id', $genres);
-			}
+    public function applyFilters (bool $apply = true): self
+    {
+        if ($apply) {
+            $languages = Str::split(',', request('language'), true);
+            if (count($languages) > 0) {
+                $this->query->whereHas('audios', function (Builder $q) use ($languages) {
+                    $q->whereIn('video_language_id', $languages);
+                });
+            }
+            $genres = Str::split(',', request('genre'), true);
+            if (count($genres) > 0) {
+                $this->query->whereIn('genre_id', $genres);
+            }
 
-			$subscription = Str::split(',', request('type'), true);
-			if (count($subscription) > 0) {
-				$this->query->whereIn('subscription_type', $subscription);
-			}
+            $subscription = Str::split(',', request('type'), true);
+            if (count($subscription) > 0) {
+                $this->query->whereIn('subscription_type', $subscription);
+            }
 
-			$rating = (request('explicit', 0) == 1) ? ['G', 'PG', 'PG-13', 'R', 'NC-17'] : ['G', 'PG', 'PG-13', 'R'];
-			$this->query->whereIn('pg_rating', $rating);
-		}
-		return $this;
-	}
+            $rating = (request('explicit', 0) == 1) ? ['G', 'PG', 'PG-13', 'R', 'NC-17'] : ['G', 'PG', 'PG-13', 'R'];
+            $this->query->whereIn('pg_rating', $rating);
+        }
+        return $this;
+    }
 }
