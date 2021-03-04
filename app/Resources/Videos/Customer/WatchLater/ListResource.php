dc4bc9e8579a8f42e691fa0ca071bc8e05c5502b
younggeeksin <raju@younggeeks.in>
2021-03-04 13:05:33 +0530

    Updates

--- app/Resources/Videos/Customer/WatchLater/ListResource.php
+++ app/Resources/Videos/Customer/WatchLater/ListResource.php
@@ -7,18 +7,18 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class ListResource extends JsonResource
 {
-	public function toArray ($request)
-	{
-		return [
-			'id' => $this->id,
-			'title' => $this->title,
-			'description' => $this->description,
-			'slug' => $this->slug,
-			'type' => $this->type,
-			'duration' => $this->duration,
-			'released' => $this->released,
-			'director' => $this->director,
-			'trailer' => Uploads::existsUrl($this->trailer),
+    public function toArray ($request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'slug' => $this->slug,
+            'type' => $this->type,
+            'duration' => $this->duration,
+            'released' => $this->released,
+            'director' => $this->director,
+            'trailer' => Uploads::existsUrl($this->trailer),
 			'rating' => $this->rating,
 			'poster' => Uploads::existsUrl($this->poster),
 			'backdrop' => Uploads::existsUrl($this->backdrop),
