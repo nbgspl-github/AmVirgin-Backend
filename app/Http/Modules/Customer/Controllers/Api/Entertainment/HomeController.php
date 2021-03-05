12c468324b0aa9747804692816df4942af4beec5
younggeeksin <raju@younggeeks.in>
2021-03-05 13:29:06 +0530

    Updates

--- app/Http/Modules/Customer/Controllers/Api/Entertainment/HomeController.php
+++ app/Http/Modules/Customer/Controllers/Api/Entertainment/HomeController.php
@@ -50,6 +50,7 @@ class HomeController extends \App\Http\Modules\Customer\Controllers\Api\ApiContr
 //                ->isNotTranscoding()
                 ->section($section->id)
                 ->take($section->max_items)
+                ->dump()
 //                ->applyFilters(true)
                 ->get();
             $contents = \App\Http\Modules\Customer\Resources\Entertainment\VideoSectionResource::collection($contents);
